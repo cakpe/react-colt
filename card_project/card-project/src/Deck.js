280fcfa7c2bff44f103b2b415457b2e4de59b865
Chibuzo o Akpe <akpechibuzo@gmail.com>
2022-12-22 19:06:27 -0500

    created deck component

---
+++ card_project/card-project/src/Deck.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import React, { Component } from 'react'
+
+const BASE_API_URL = "https://deckofcardsapi.com/api/deck";
+
+class Deck extends Component {
+    state = {
+        deck: null,
+        drawn: []
+    }
+
+    getCard = async () => {
+        //request card from api
+        let deck_id = this.state.deck.deck_id;
+        let cardUrl = `${BASE_API_URL}/${deck_id}/draw/`;
+
+        try {
+            let cardRes = await axios.get(cardUrl);
+            if(!cardRes.data.success) {
+                throw new Error("No card remaining");
+            }
+
+            //store in state array
+            let card = cardRes.data.cards[0];
+            this.setState(state => ({
+                drawn: [
+                    ...state.drawn, 
+                    {
+                        id: card.code,
+                        image: card.image,
+                        name: `${card.value} of ${card.suit}`
+                    }
+                ]
+            }));
+        } catch(err) {
+            alert(err);
+        }
+    }
+    
+    //the non-async way
+    // getCard = () => {
+    //     //request card from api
+    //     let deck_id = this.state.deck.deck_id;
+    //     let cardUrl = `${BASE_API_URL}/${deck_id}/draw/`;
+
+    //     let cardRes;
+    //     axios.get(cardUrl).then((response) => {
+    //         cardRes = response;
+            
+    //         //store in state array
+    //         let card = cardRes.data.cards[0];
+    //         this.setState(state => ({
+    //             drawn: [
+    //                 ...state.drawn, 
+    //                 {
+    //                     id: card.code,
+    //                     image: card.image,
+    //                     name: `${card.value} of ${card.suit}`
+    //                 }
+    //             ]
+    //         }));
+    //     })
+    //     .catch((err) => console.log(err));
+    // }
+
+    //think of this as useEffect in react hooks
+    async componentDidMount() {
+        let deck = await axios.get(`${BASE_API_URL}/new/shuffle`);
+        this.setState({ deck: deck.data});
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>Card Dealer</h1>
+                <button onClick={this.getCard}>Get Card!</button>
+            </div>
+        );
+    }
+}
+
+export default Deck;
