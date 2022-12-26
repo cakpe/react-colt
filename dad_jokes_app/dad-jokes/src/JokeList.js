db24cc734a13b369a586e9d68c53199b9824eb5e
Chibuzo o Akpe <akpechibuzo@gmail.com>
2022-12-26 09:12:04 -0500

    Built joke list component and styled it

---
+++ dad_jokes_app/dad-jokes/src/JokeList.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import axios from "axios"
+import './JokeList.css'
+
+class JokeList extends Component {
+
+    static defaultProps = {
+        numJokes: 10
+    }
+
+    state = {
+        jokes: []
+    }
+
+    async componentDidMount() {
+        let jokes = [];
+        while(jokes.length < this.props.numJokes) {
+            let res = await axios.get("https://icanhazdadjoke.com/", {headers: {Accept: "application/json"}});
+            jokes.push(res.data.joke);
+        }
+
+        this.setState({ jokes: jokes });
+    }
+
+    render() {
+        return (
+            <div className='JokeList'>
+                <div className='JokeList-sidebar'>
+                    <h1 className='JokeList-title'><span>Dad</span> Jokes</h1>
+                    <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
+                    <button className='JokeList-getmore'>New Jokes</button>
+                </div>
+
+                <div className='JokeList-jokes'>
+                    {this.state.jokes.map(joke => {
+                        return <li>{joke}</li>
+                    })}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default JokeList;
