644fc53ab8ffa169261f9cbdd9731a3c19a71bfc
Chibuzo o Akpe <akpechibuzo@gmail.com>
2022-12-29 15:35:14 -0500

    setup routes

--- react_router/react-router/src/App.js
+++ react_router/react-router/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Book from './pages/Book';
+import BookList from './pages/BookList';
+import Contact from './pages/Contact';
+import NewBook from './pages/NewBook';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/book" element={<Book />}/>
+      <Route path="/booklist" element={<BookList />}/>
+      <Route path="/contact" element={<Contact />}/>
+      <Route path="/newbook" element={<NewBook />}/>
+      <Route path="/notfound" element={<NotFound />}/>
+    </Routes>
   );
 }
 
