1a0fea4061d83c75e32e706ce4f9589f70d64e5b
Chibuzo o Akpe <akpechibuzo@gmail.com>
2022-12-29 15:52:59 -0500

    added nav bar and mapped to routes

--- react_router/react-router/src/App.js
+++ react_router/react-router/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Route, Routes} from 'react-router-dom';
+import {Link, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Book from './pages/Book';
@@ -10,15 +10,28 @@ import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/about" element={<About />}/>
-      <Route path="/book" element={<Book />}/>
-      <Route path="/booklist" element={<BookList />}/>
-      <Route path="/contact" element={<Contact />}/>
-      <Route path="/newbook" element={<NewBook />}/>
-      <Route path="/notfound" element={<NotFound />}/>
-    </Routes>
+    <>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/book">Book</Link></li>
+          <li><Link to="/booklist">BookList</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/newbook">NewBook</Link></li>
+          <li><Link to="/notfound">NotFound</Link></li>
+        </ul>
+      </nav>
+      <Routes> {/*only the elements wrapped in routes are re-rendered. the nav bar remains static on the page */}
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/book" element={<Book />}/>
+        <Route path="/booklist" element={<BookList />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/newbook" element={<NewBook />}/>
+        <Route path="/notfound" element={<NotFound />}/>
+      </Routes>
+    </>
   );
 }
 
