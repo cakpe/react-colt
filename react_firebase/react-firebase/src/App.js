20fcf4c1174d091986c2fb4c1c0ad868dfab2cec
Chibuzo o Akpe <akpechibuzo@gmail.com>
2022-12-30 07:15:50 -0500

    getting data from firestore

--- react_firebase/react-firebase/src/App.js
+++ react_firebase/react-firebase/src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+import { useState, useEffect } from "react"
+import { db } from './firebase-config';
+import { collection, getDocs } from 'firebase/firestore'
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  //below is how to point to a collection in firestore
+  const usersCollectionRef = collection(db, "users");
+
+  //remember that to call an async function in a useEffect, you have to create it in the useEffect.
+  useEffect(() => {
+    const getUsers = async() => {
+      try {
+        const data = await getDocs(usersCollectionRef);
+        setUsers(data.docs.map(doc => (
+          {...doc.data(), id: doc.id}
+        )))
+      } catch(e) {
+        alert(e);
+      }
+    };
+
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
-      
+      {users.map(user => {
+        return <div key={user.id}> 
+          <h1>Name: {user.name}</h1>
+          <p>Age: {user.age}</p>
+        </div>
+      })}
     </div>
   );
 }
